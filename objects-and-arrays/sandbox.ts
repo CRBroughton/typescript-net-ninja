c6de870299abc6f8c2cb59f11b904f9dcf0d6592
CRBroughton <crbroughton@posteo.uk>
2021-10-16 11:32:23 +0100

    added arrays and objects examples

--- objects-and-arrays/sandbox.ts
+++ objects-and-arrays/sandbox.ts
@@ -5,5 +5,29 @@ let names = ['luigi', 'mario', 'yoshi']
 // only contains strings, unless explicitly allowed
 // names.push(3)
 
+// Mixed arrays
+
+let mixed = ['ken', 4, 'chun-li', 8, 9]
+mixed.push('ryu')
+mixed.push(10)
+mixed[0] = 3
+
 
 // Objects
+
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+}
+
+ninja.age = 40
+ninja.name = 'ryu'
+
+// If you override an object, the structure of that object need to match
+// The below example won't work because it is missing age
+
+// ninja = {
+//     name: 'yoshi',
+//     belt: 'orange',
+// }
