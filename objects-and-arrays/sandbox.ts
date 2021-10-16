4ad30c624f3512e9455b4de35eadf205a1db97fb
CRBroughton <crbroughton@posteo.uk>
2021-10-16 11:27:07 +0100

    added push example

---
+++ objects-and-arrays/sandbox.ts
@@ -0,0 +1,9 @@
+// Arrays
+let names = ['luigi', 'mario', 'yoshi']
+
+// It is not possible to push a number to an array that
+// only contains strings, unless explicitly allowed
+// names.push(3)
+
+
+// Objects
