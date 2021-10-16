dc10e03138f39de2237742ea7827714e46e3bc70
CRBroughton <crbroughton@posteo.uk>
2021-10-16 11:19:21 +0100

    added basic types

---
+++ type-basics/sandbox.ts
@@ -0,0 +1,15 @@
+// These variables are inferred types: Typescript is inferring the types based on what is set
+
+let character  = 'mario'
+let age = 30
+let isBlackBelt = false
+
+// character cannot be set to 2, because character is already a string
+// character = 2
+
+// age however can be set to another number, because it already is a number
+age = 40
+
+// isBlackBelt can be set to true, because it is already a boolean
+// however it cannot be set to anything else
+isBlackBelt = true
