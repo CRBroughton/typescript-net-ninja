f462b28369f7b8369abe5b162a9a239b07551712
CRBroughton <crbroughton@posteo.uk>
2021-10-16 12:48:06 +0100

    added files for type-aliases

---
+++ type-aliases/src/sandbox.ts
@@ -0,0 +1,32 @@
+// Typescript will infer that the below is a function type
+// You can also declare the type:
+
+// let greet: Function
+
+// notice how the Function type uses a capital letter
+
+let greet = () => {
+    console.log('hello, world!')
+}
+
+// Optional types / arguments
+
+// Option arguments are prefixed with a ? mark
+const add = (a: number, b: number, c?: number | string) => {
+    console.log(a + b, c)
+}
+
+// If we wanted a default value, we could do the following:
+
+const addDefault = (a: number, b: number, c: number | string = 10) => {
+    console.log(a + b, c)
+}
+
+// making c defaulting to 10 unless specified
+
+// it is also possible to specify the return type
+const returnType = (a: number, b: number): number => {
+    return a + b
+}
+
+// when a return value is non-existent, the function will return a void type
