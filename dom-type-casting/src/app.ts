f961aab72d94eaddf6460e962c64a67662652267
CRBroughton <crbroughton@posteo.uk>
2021-10-23 09:45:05 +0100

    added inputs

--- dom-type-casting/src/app.ts
+++ dom-type-casting/src/app.ts
@@ -14,3 +14,14 @@ console.log(anchor.href);
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
 console.log(form.children);
+
+
+// inputs
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+
+const toFrom = document.querySelector('#toFrom') as HTMLInputElement;
+
+const details = document.querySelector('#details') as HTMLInputElement;
+
+const amount = document.querySelector('#amount') as HTMLInputElement;
