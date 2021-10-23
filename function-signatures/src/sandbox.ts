a3ce34e1817d6d26934390090b0a565040f3f42b
CRBroughton <crbroughton@posteo.uk>
2021-10-23 09:17:10 +0100

    added example 1

--- function-signatures/src/sandbox.ts
+++ function-signatures/src/sandbox.ts
@@ -1,2 +1,10 @@
-let greet: Function;
+// Example 1
 
+// Function signatures allow use to define a functions
+// parameters and return type
+
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, greeting: string) => {
+    console.log(name, greeting);
+}
