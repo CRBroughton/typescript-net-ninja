099ef9cd4ced51172c4bf2346da8ca344648f2d0
CRBroughton <crbroughton@posteo.uk>
2021-10-23 09:10:24 +0100

    added second type alias

--- type-aliases/src/sandbox.ts
+++ type-aliases/src/sandbox.ts
@@ -2,10 +2,13 @@
 // This is a type alias
 type StringOrNum = string | number;
 
+// This is also a type alias, but is using another type alias
+type objWithName = { name: string, uid: StringOrNum}
+
 const logDetails = (uid: StringOrNum, item: string) => {
     console.log(`${item} has a uid of ${uid}`);
 }
 
-const greet = (user: { name: string, uid: StringOrNum}) => {
+const greet = (user: objWithName) => {
     console.log(`${user.name} says hello`);
 }
