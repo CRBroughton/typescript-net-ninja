ea516647036a999a66cc620bb4b054770122c6ab
CRBroughton <crbroughton@posteo.uk>
2021-10-31 07:54:34 +0000

    reformatted constructor

--- dom-type-casting/src/app.ts
+++ dom-type-casting/src/app.ts
@@ -6,15 +6,18 @@ class Invoice {
     // private: Can only be read inside of the class.
     // public: This is the default behaviour, can be read and modified anywhere.
 
-    client: string;
-    details: string;
-    amount: number;
+    // client: string;
+    // details: string;
+    // amount: number;
 
-    constructor(c: string, d: string, a: number) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
+    constructor(
+        // this.client = c;
+        // this.details = d;
+        // this.amount = a;
+        readonly client: string,
+        private details: string,
+        public amount: number,
+    ){}
 
     format() {
         return `${this.client} owes £${this.amount} for ${this.details}`
