70cacb09ee3fd36e2a65e151ac588d3704198d4b
CRBroughton <crbroughton@posteo.uk>
2021-10-31 10:12:40 +0000

    invoices now show as list

--- dom-type-casting/src/app.ts
+++ dom-type-casting/src/app.ts
@@ -1,6 +1,7 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
+import { ListTemplate } from './classes/ListTemplate.js'
 
 // Interfaces with Classes
 
@@ -88,6 +89,10 @@ const details = document.querySelector('#details') as HTMLInputElement;
 
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
+// list template instance
+const ul = document.querySelector('ul')!
+const list = new ListTemplate(ul)
+
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
 
@@ -99,5 +104,6 @@ form.addEventListener('submit', (e: Event) => {
         doc = new Payment(toFrom.value, details.value, amount.valueAsNumber)
     }
 
-    console.log(doc)
+    list.render(doc, type.value, 'end')
+
 })
